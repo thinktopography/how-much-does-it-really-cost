926217cd12b69bd68ecc889afe9fa7e7624cd17b
Greg Kops <greg@thinktopography.com>
2017-04-23 06:30:07 -0400

    edploy script

---
+++ deploy.sh
@@ -0,0 +1,1 @@
+rsync -e 'ssh -i /Users/mochini/.ssh/thinktopography'  -a dist/* root@preview.thinktopography.com:/var/www/vhosts/preview.thinktopography.com/httpdocs/how-much-does-it-really-cost
--- src/index.js
+++ src/index.js
@@ -5,6 +5,107 @@ import data from './data.json'
 import cheerio from 'cheerio'
 import _ from 'lodash'
 
+const pallettes = {
+  h: [
+    { min: 0, max: 0, color: 'white' },
+    { min: 1, max: 40, color: 'red' },
+    { min: 41, max: 45, color: 'orange' },
+    { min: 46, max: 50, color: 'yellow' },
+    { min: 51, max: 55, color: 'green' },
+    { min: 56, max: 60, color: 'blue' },
+    { min: 66, max: 75, color: 'purple' },
+    { min: 76, max: 80, color: 'violet' },
+    { min: 81, max: 85, color: 'brown' },
+    { min: 86, max: 90, color: 'grey' },
+    { min: 90, max: 100, color: 'black' }
+  ],
+  t: [
+    { min: 0, max: 0, color: 'white' },
+    { min: 1, max: 20, color: 'red' },
+    { min: 21, max: 30, color: 'orange' },
+    { min: 31, max: 40, color: 'yellow' },
+    { min: 41, max: 50, color: 'green' },
+    { min: 51, max: 60, color: 'blue' },
+    { min: 61, max: 100, color: 'purple' }
+
+  ],
+  ht: [
+    { min: 0, max: 0, color: 'white' },
+    { min: 1, max: 15, color: 'red' },
+    { min: 16, max: 20, color: 'orange' },
+    { min: 21, max: 25, color: 'yellow' },
+    { min: 26, max: 30, color: 'green' },
+    { min: 31, max: 100, color: 'blue' }
+  ]
+}
+
+const sections = [
+  {
+    label: 'Housing and Transportation Percentage of Income',
+    data: {
+      ht_ami: {
+        label: 'Average Income',
+        unit: '%'
+      },
+      ht_80ami: {
+        label: 'Modest Income',
+        unit: '%'
+      }
+    }
+  },{
+    label: 'Housing Percentage of Income',
+    data: {
+      h_ami: {
+        label: 'Average Income',
+        unit: '%'
+      },
+      h_80ami: {
+        label: 'Modest Income',
+        unit: '%'
+      }
+    }
+  },{
+    label: 'Transportation Percentage of Income',
+    data: {
+      t_ami: {
+        label: 'Average Income',
+        unit: '%'
+      },
+      t_80ami: {
+        label: 'Modest Income',
+        unit: '%'
+      }
+    }
+  },{
+    label: 'Annual Transportation Cost',
+    data: {
+      t_cost_ami: {
+        label: 'Average Income',
+        unit: '$'
+      },
+      t_cost_80a: {
+        label: 'Modest Income',
+        unit: '$'
+      }
+    }
+  },{
+    label: 'Other',
+    data: {
+      vmt_per_hh: {
+        label: 'Annual Vehicle Miles Traveled per Household',
+        unit: 'mi'
+      },
+      co2_per_hh: {
+        label: 'Annual CO2 released per Household',
+        unit: 'PPM'
+      },
+      h_cost: {
+        label: 'Average Monthly Housing Cost per Household',
+        unit: '$'
+      }
+    }
+  }
+]
 class Main extends React.Component {
 
   constructor(props) {
@@ -12,30 +113,46 @@ class Main extends React.Component {
     this.ranges = {}
     this.blockgroups = []
     this.marker = null
+    this.state = {
+      type: 'h',
+      metric: 'h_ami',
+      details: null,
+      legend: null
+    }
   }
 
   render() {
+    const { details, legend, metric } = this.state
     return (
       <div className="wdic">
         <div className="wdic-header">
-          <div className="row">
-            <div className="col-lg-12 text-center">
-              <div className="btn-group">
-                <button type="button" className="btn btn-default" onClick={ this._changeMetric.bind(this, 'h_ami')}>h_ami</button>
-                <button type="button" className="btn btn-default" onClick={ this._changeMetric.bind(this, 'h_80ami')}>h_80ami</button>
-                <button type="button" className="btn btn-default" onClick={ this._changeMetric.bind(this, 'h_nmi')}>h_nmi</button>
-                <button type="button" className="btn btn-default" onClick={ this._changeMetric.bind(this, 'ht_ami')}>ht_ami</button>
-                <button type="button" className="btn btn-default" onClick={ this._changeMetric.bind(this, 'ht_80ami')}>ht_80ami</button>
-                <button type="button" className="btn btn-default" onClick={ this._changeMetric.bind(this, 'ht_nmi')}>ht_nmi</button>
-              </div>
+          <div className="metric">
+            <h5>Housing</h5>
+            <div className="btn-group">
+              <button type="button" className={`btn ${metric === 'h_ami' ? 'btn-danger' : 'btn-default'}`} onClick={ this._changeMetric.bind(this, 'h', 'h_ami')}>Average Income</button>
+              <button type="button" className={`btn ${metric === 'h_80ami' ? 'btn-danger' : 'btn-default'}`} onClick={ this._changeMetric.bind(this, 'h', 'h_80ami')}>Modest Income</button>
+            </div>
+          </div>
+          <div className="metric">
+            <h5>Transportation</h5>
+            <div className="btn-group">
+              <button type="button" className={`btn ${metric === 't_ami' ? 'btn-danger' : 'btn-default'}`} onClick={ this._changeMetric.bind(this, 't', 't_ami')}>Average Income</button>
+              <button type="button" className={`btn ${metric === 't_80ami' ? 'btn-danger' : 'btn-default'}`} onClick={ this._changeMetric.bind(this, 't', 't_80ami')}>Modest Income</button>
+            </div>
+          </div>
+          <div className="metric">
+            <h5>Housing & Transportation</h5>
+            <div className="btn-group">
+              <button type="button" className={`btn ${metric === 'ht_ami' ? 'btn-danger' : 'btn-default'}`} onClick={ this._changeMetric.bind(this, 'ht', 'ht_ami')}>Average Income</button>
+              <button type="button" className={`btn ${metric === 'ht_80ami' ? 'btn-danger' : 'btn-default'}`} onClick={ this._changeMetric.bind(this, 'ht', 'ht_80ami')}>Modest Income</button>
             </div>
           </div>
         </div>
         <div className="wdic-body">
           <div className="wdic-sidebar">
             <div className="form">
-              <h2>Find My House</h2>
-              <p>Enter your address below to find out the specific details for your home</p>
+              <h4>What Does it Really Cost?</h4>
+              <p>VHS actually. Quinoa vinyl sartorial, pour-over cronut vexillologist VHS. Af microdosing gentrify bicycle rights, marfa affogato tote bag kickstarter readymade authentic air plant craft beer poke VHS. Selvage letterpress af, lumbersexual snackwave shoreditch butcher marfa. Small batch kombucha offal kinfolk hella, vice hexagon literally mumblecore</p>
               <form onSubmit={ this._handleSubmit.bind(this) }>
                 <div className="form-group">
                   <input type="text" className="form-control" ref="street" id="street" placeholder="Street" />
@@ -49,21 +166,44 @@ class Main extends React.Component {
                 <div className="form-group">
                   <input type="text" className="form-control" ref="zip" id="zip" placeholder="Zip" />
                 </div>
-                <button type="submit" className="btn btn-default btn-block btn-danger">Locate</button>
+                <button type="submit" className="btn btn-default btn-block btn-danger">Lookup my House</button>
               </form>
             </div>
           </div>
           <div className="wdic-map" ref="map" />
+          <div className="wdic-details">
+            { details &&
+              <div>
+                <h4>BLOCKGROUP { details.ID }</h4>
+                { sections.map(section => (
+                  <table>
+                    <tr>
+                      <th colSpan="2">{ section.label }</th>
+                    </tr>
+                    { Object.keys(section.data).map(key => (
+                      <tr>
+                        <td>{ section.data[key].label }</td>
+                        <td>
+                          { section.data[key].unit === '$' ? section.data[key].unit : null }
+                          { details[key] }
+                          { section.data[key].unit !== '$' ? section.data[key].unit : null }
+                        </td>
+                      </tr>
+                    )) }
+                  </table>
+                ))}
+              </div>
+            }
+          </div>
         </div>
         <div className="wdic-footer">
-          { [0,1,2,3,4,5,6,7,8,9].map(index => {
-            return (
+          { legend &&
+            legend.map((segment, index) => (
               <div className="segment" key={`metric${index}`}>
-                <div className={`key segment${index}`}></div>
-                <span ref={`metric${index}`} />
+                <div className={`key`} style={{background: segment.color}}></div>
+                <span>{ segment.min } - { segment.max }</span>
               </div>
-            )
-          })}
+            ))}
         </div>
       </div>
     )
@@ -72,7 +212,7 @@ class Main extends React.Component {
   componentDidMount() {
     this._prepareData()
     this._drawMap()
-    this._changeMetric('h_ami')
+    this._changeMetric('h', 'h_ami')
   }
 
   _prepareData() {
@@ -80,8 +220,8 @@ class Main extends React.Component {
       const coordinates = feature.geometry.coordinates[0].map(coordinates => {
         return { lat: coordinates[1], lng: coordinates[0] }
       })
-      const table = cheerio.load(feature.properties.description)
-      const rows = table('table table tr')
+      const parsed = cheerio.load(feature.properties.description)
+      const rows = parsed('table table tr')
       const metadata = Object.keys(rows).reduce((metadata, index) => {
         const row = rows[index]
         if(row && row.children && row.children[1]) {
@@ -98,6 +238,7 @@ class Main extends React.Component {
         }
         return metadata
       }, {})
+
       return { coordinates, metadata }
     })
     this.ranges = Object.keys(this.ranges).reduce((ranges, key) => {
@@ -122,29 +263,38 @@ class Main extends React.Component {
     this.blockgroups.map((blockgroup, index) => {
       this.blockgroups[index].polygon = new google.maps.Polygon({
         path: blockgroup.coordinates,
-        strokeColor: '#FF0000',
-        strokeOpacity: 0.8,
+        strokeOpacity: 1,
         strokeWeight: 1,
-        fillColor: '#FF0000',
         fillOpacity: 0.35
       })
       this.blockgroups[index].polygon.setMap(this.map)
+
+      this.blockgroups[index].polygon.addListener('click', this._handleClick.bind(this, this.blockgroups[index]))
     })
   }
 
-  _changeMetric(key) {
+  _handleClick(blockgroup) {
+    this._changeMetric(this.state.type, this.state.metric)
+    blockgroup.polygon.setOptions({ fillOpacity: 1 });
+    this.setState({
+      details: blockgroup.metadata
+    })
+  }
+
+  _changeMetric(type, metric) {
+    const legend = pallettes[type]
     this.blockgroups.map((blockgroup, index) => {
-      const value = blockgroup.metadata[key]
-      const step = this.ranges[key].step
-      const fillOpacity = Math.floor(value / step) / 10
-      blockgroup.polygon.setOptions({ fillOpacity });
+      const value = blockgroup.metadata[metric]
+      const color = legend.filter(segment => {
+        return value >= segment.min && value <= segment.max
+      })[0].color
+      blockgroup.polygon.setOptions({
+        fillOpacity: 0.35,
+        strokeColor: color,
+        fillColor: color
+      })
     })
-    const segments = [0,1,2,3,4,5,6,7,8,9]
-    segments.map(segment => {
-      const min = this.ranges[key].min + this.ranges[key].step * segment
-      const max = (min + this.ranges[key].step).toFixed(2) - .01
-      this.refs[`metric${segment}`].innerHTML = `${min.toFixed(2)} - ${max.toFixed(2)}`
-    })
+    this.setState({ type, metric, legend })
   }
 
   _handleSubmit(e) {
